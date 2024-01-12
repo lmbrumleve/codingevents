602d866eda9615cf7670146653a6023e386ef940
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-01-12 16:33:26 -0600

    initial commit

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,31 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    @GetMapping
+    public String events(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("TopCoder");
+        events.add("Google Code Jam");
+        events.add("Microsoft Imagine Cup");
+
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+}
