fe6a94ba8e956717ff80f85b10e920e2a51486a7
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-01-12 18:01:53 -0600

    create and render a form - done

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,12 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-    @GetMapping
-    public String events(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("TopCoder");
-        events.add("Google Code Jam");
-        events.add("Microsoft Imagine Cup");
+    private static List<String> events = new ArrayList<>();
 
+    @GetMapping
+    public String displayALlEvents(Model model) {
         model.addAttribute("events", events);
+
         return "events/index";
     }
 
@@ -26,6 +26,11 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 
 }
